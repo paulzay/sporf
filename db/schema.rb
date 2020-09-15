0da0d42b01ff5c5bbfa120ea06f8c23518130ff3
paulzay <paulotieno2@gmail.com>
2020-09-15 14:52:39 +0300

    scaffold and migrate categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_110642) do
+ActiveRecord::Schema.define(version: 2020_09_15_115013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2020_09_15_110642) do
     t.string "title"
     t.string "text"
     t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
