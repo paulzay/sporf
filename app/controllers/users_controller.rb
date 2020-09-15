d577e46fd19bf141ac49f90581e542fb9725b63b
paulzay <paulotieno2@gmail.com>
2020-09-15 13:16:48 +0300

    build sessions and login features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :logged_in_user, only: [:show]
+
 	def index
 	end
 
@@ -13,6 +15,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			log_in @user
 			flash[:success] = "Account created!"
       		redirect_to @user
       	else
