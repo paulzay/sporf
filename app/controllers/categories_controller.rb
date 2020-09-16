e25d7a5590580ed4f619845c61962f595468997c
paulzay <paulotieno2@gmail.com>
2020-09-16 22:31:13 +0300

    style featured article

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.all.order('created_at DESC')
   end
 
   # GET /categories/1
