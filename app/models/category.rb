bc75a5f9d1d2123088cbc179fd318ac982a51077
paulzay <paulotieno2@gmail.com>
2020-09-17 19:14:34 +0300

    add association for most recent article in category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   has_many :articles
+
+  has_one :most_recent_article, -> do
+    merge(Article.most_recent_by_category)
+  end, class_name: "Article", inverse_of: :category
 end
