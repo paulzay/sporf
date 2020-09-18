1b1b1a90a7361d8e627a1bed32d36ed92a06964a
paulzay <paulotieno2@gmail.com>
2020-09-18 13:31:59 +0300

    style the articles index and show

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,10 @@
 module ArticlesHelper
+	def like_dislike_btn
+		pre_vote = @article.votes.find { |vote| vote.user_id == current_user.id}
+		if pre_vote 
+		  button_to 'Unlike', article_vote_path(@article, pre_vote), method: :delete, class: "btn btn-secondary"
+		else
+		  button_to 'Like', article_votes_path(@article), method: :post, class: "btn btn-secondary" 
+		end 
+	end
 end
