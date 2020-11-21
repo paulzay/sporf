48b100cc7d65ebd46c475894c3e19726d0c06e05
paulzay <paulotieno2@gmail.com>
2020-11-21 23:25:34 +0300

    add config for redcapet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def categories
     Category.all.map { |c| [c.name, c.id] }
   end
+  
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, hard_wrap: true, filter_html: true, autolink: true, no_intraemphasis: true)
+    return markdown.render(text).html_safe
+  end
 end
